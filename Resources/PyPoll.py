dfb6d648b5cfb4fdc35e68c2a8c70b54579cd5be
JRayHunt <71520850+JRayHunt@users.noreply.github.com>
2020-10-16 16:06:40 -0500

    Create PyPoll.py

---
+++ Resources/PyPoll.py
@@ -0,0 +1,20 @@
+# Get the data we need
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Print the file object.
+     print(election_data)
+     
+# Total number of votes cast
+
+# A complete list of candidates who received votes
+
+# Total number of votes each candidate received
+
+# Percentage of votes each candidate won
+
+# The winner of the election based on popular vote
